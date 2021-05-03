5ea9f3156d1e3c87ac331e18a1baddd60017d3c1
Jiaming Zhang <jason721b@gmail.com>
2021-05-02 23:03:18 -0700

    Add largest-divisible-subset

---
+++ online-judges/leetcode/largest-divisible-subset/solution.cpp
@@ -0,0 +1,60 @@
+#include <vector>
+#include <algorithm>
+#include <iostream>
+#include <unordered_set>
+#include <queue>
+
+using namespace std;
+
+class Solution {
+public:
+    vector<int> largestDivisibleSubset(vector<int>& nums) {
+        // Samllest number come first
+        sort(nums.begin(), nums.end());
+        
+        vector<int> largestSet;
+        vector<int> wipSet;
+        largestDivisibleSubsetInternal(nums, /* startIndex= */ 0, wipSet, largestSet);
+        
+        return largestSet;
+    }
+private:
+    void largestDivisibleSubsetInternal(
+        vector<int>& nums, int startIndex, vector<int>& wipSet, vector<int>& largestSet) {
+        
+        if (startIndex >= nums.size()) {
+            if (wipSet.size() > largestSet.size()) {
+                largestSet = wipSet; // COPY
+            }
+        }
+        
+        // Find the next number to put into wipSet
+        for (int i = startIndex; i < nums.size(); i++) {
+            if (wipSet.size() == 0) {
+                wipSet.push_back(nums[i]);
+                largestDivisibleSubsetInternal(nums, i+1, wipSet, largestSet);
+                wipSet.pop_back();
+            } else if (nums[i] % wipSet.back() == 0) {
+                wipSet.push_back(nums[i]);
+                largestDivisibleSubsetInternal(nums, i+1, wipSet, largestSet);
+                wipSet.pop_back();
+            }
+        }
+    }
+};
+
+vector<int> makeInput()
+{
+    return vector<int> {2,3,4,9,8};
+}
+
+int main()
+{
+    Solution s;
+    auto input = makeInput();
+    vector<int> nums = s.largestDivisibleSubset(input);
+    for (int num : nums) {
+        cout << num << " ";
+    }
+    cout << endl;
+}
