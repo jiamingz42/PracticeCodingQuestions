57f9d5933556df268dac817380c4f57cb0bde2f2
Jiaming Zhang <jason721b@gmail.com>
2021-05-05 21:56:50 -0700

    Add  largest-divisible-subset

--- online-judges/leetcode/largest-divisible-subset/solution.cpp
+++ online-judges/leetcode/largest-divisible-subset/solution.cpp
@@ -75,10 +75,15 @@ vector<int> makeInput()
     return vector<int>{2, 3, 4, 9, 8};
 }
 
+vector<int> makeInput2()
+{
+    return vector<int>{1, 2, 4, 8, 16, 32, 64, 128, 256, 512, 1024, 2048, 4096, 8192, 16384, 32768, 65536, 131072, 262144, 524288, 1048576, 2097152, 4194304, 8388608, 16777216, 33554432, 67108864, 134217728, 268435456, 536870912, 1073741824};
+}
+
 int main()
 {
     Solution s;
-    auto input = makeInput();
+    auto input = makeInput2();
     vector<int> nums = s.largestDivisibleSubset(input);
     for (int num : nums)
     {
