7108869f02f3082387bb35e6c9eda4dcad6e83e2
Jiaming Zhang <jason721b@gmail.com>
2021-05-05 22:10:39 -0700

    Add _tempalte

--- online-judges/leetcode/_template_/main.cpp
+++ online-judges/leetcode/_template_/main.cpp
@@ -1,16 +1,42 @@
 #include <vector>
 #include <algorithm>
 #include <iostream>
+#include <cstdint>
 
 using namespace std;
 
-// TODO: Solution 
+class Solution {
+public:
+    int jump(vector<int>& nums) {
+        if (nums.size() <= 1) {
+            return nums.size();
+        }
+
+        vector<int> dp(nums.size(), INT_MAX);
+        dp[0] = 0;
+
+        for (unsigned long i = 1; i < nums.size(); i++) {
+            for (int j = 0; j < i; j++) {
+                if (i - j <= nums[j]) {
+                    dp[i] = min(dp[i], dp[j]+1);
+                }
+            }
+        }
+        return dp[nums.size()-1];
+    }
+};
 
 int main()
 {
     Solution s;
-    vector<vector<int>> courses = makeInput();
-    int res = s.scheduleCourse(courses);
-    printf("res = %d", res);
+
+    vector<int> input1 = {2,3,1,1,4};
+    int res = s.jump(input1);
+
+    cout << res << endl;
+
+    vector<int> input2 = {2,3,0,1,4};
+    res = s.jump(input1);
+    cout << res << endl;
 }
 
