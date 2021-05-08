fc5455fa44732baa5398eb12c6dec9523017f284
Jiaming Zhang <jason721b@gmail.com>
2021-05-07 22:31:10 -0700

    Add 1st attemp for jump game 3

---
+++ online-judges/leetcode/jump-game-3/solution.cpp
@@ -0,0 +1,31 @@
+// Error
+class Solution {
+public:
+    bool canReach(vector<int>& arr, int start) {
+        unordered_set<int> visited;
+        visited.insert(start);
+        
+        queue<int> q;
+        q.push(start);
+        
+        while (!q.empty()) {
+            int i = q.front(); q.pop();
+            if (arr[i] == 0) {
+                return true;
+            }
+            
+            int j = i - arr[i];
+            if (j >= 0 && visited.find(j) == visited.end()) {
+                visited.insert(j);
+                q.push(j);
+            }
+            
+            int k = i + arr[i];
+            if (k < arr.size() && visited.find(j) == visited.end()) {
+                visited.insert(k);
+                q.push(k);
+            }
+        }
+        return false;
+    }
+};
