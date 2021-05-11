d4ffe56e3baa560e632716f187e4507460e5d203
Jiaming Zhang <jason721b@gmail.com>
2021-05-11 14:17:26 -0700

    bug fix: the solution passes the tests

--- online-judges/leetcode/remove-duplicate-letters/solution.cpp
+++ online-judges/leetcode/remove-duplicate-letters/solution.cpp
@@ -1,17 +1,3 @@
-#include <string>
-#include <unordered_map>
-#include <unordered_set>
-#include <sstream>
-
-using namespace std;
-
-/** 
- * Incorrect Solution
- * 
- * Input: "cbacdcbc"
- * Output: "adbc"
- * Expected: "acdb"
- */
 class Solution {
 public:
     string removeDuplicateLetters(string s) {
@@ -20,24 +6,35 @@ public:
             counter[ch]++;
         }
         
-        unordered_set<char> added;
+        bool added[26];
+        for (int i = 0; i < 26; i++) {
+            added[i] = false;
+        }
+        
         ostringstream ss;
         
         int min_char_i = 0;
-        for (int i = 0; i < s.size(); i++) {
-            char ch = s[i];
-            if (added.find(ch) != added.end()) {
-                continue;
-            }
-            
-            // strictly smaller than so if there are multiple occurence of the smallest char, the leftmost one is used
-            if (ch < s[min_char_i]) {
-                min_char_i = i;
-            }
-            if (--counter[ch] == 0) {
-                ss << ch;
-                added.insert(ch);
-                min_char_i = i+1;
+        while (min_char_i < s.size()) {
+            for (int i = min_char_i; i < s.size(); i++) {
+                char ch = s[i];
+                if (added[ch-'a']) {
+                    if (min_char_i == i) min_char_i++;
+                    continue;
+                }
+
+                // strictly smaller than so if there are multiple occurence of the smallest char, the leftmost one is used
+                if (ch < s[min_char_i]) {
+                    min_char_i = i;
+                }
+                if (--counter[ch] == 0) {
+                    ss << s[min_char_i];
+                    added[s[min_char_i]-'a'] = true;
+                    min_char_i++;
+                    for (int j = min_char_i; j <= i; j++) {
+                        counter[s[j]]++;
+                    }
+                    break;
+                }
             }
         }
         
